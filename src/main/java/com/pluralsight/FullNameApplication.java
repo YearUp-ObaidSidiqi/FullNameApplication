e340c84e7cd0075e1bd6304c38ccb9eae32e64f2
SidiqiObaid <obaid.yearuo@gmail.com>
2024-10-05 15:04:27 -0400

    V.01 small modification

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,13 +1,36 @@
 package com.pluralsight;
 import java.util.Scanner;
 public class FullNameApplication {
+   static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
 
-        String firstName, middleName, lastName, suffix;
-
-        Scanner scanner = new Scanner(System.in);
+        String firstName, middleName, lastName, suffix, fullName;
         System.out.println("PLease Enter Your Information");
-        System.out.print("\nPLease Enter Your First Name: ");
+        firstName = getNamePromt("\nPLease Enter Your First Name: ");
+        middleName = getNamePromt("PLease Enter Your Middle Name: ");
+        lastName= getNamePromt("PLease Enter Your Last Name: ");
+        suffix= getNamePromt("PLease Enter Your Suffix: ");
+
+        if (middleName == "") {
+            if (suffix == "") {
+                System.out.println("Your Full Name is: " + firstName + " " + lastName);
+            }
+        }
+            if (suffix == "") {
+                System.out.println("Your Full Name is: " + firstName + " "+middleName+" " + lastName);
+            }
+
+    }
+
+    public static String getNamePromt(String promt){
+        System.out.println(promt);
+        String output = scanner.nextLine();
+        return output;
+
+    }
+    // Manually Typing in!
+
+            /*System.out.print("\nPLease Enter Your First Name: ");
         firstName = scanner.nextLine();
         System.out.print("PLease Enter Your Middle Name: ");
         middleName = scanner.nextLine();
@@ -15,11 +38,7 @@ public class FullNameApplication {
         lastName = scanner.nextLine();
         System.out.print("PLease Enter Your Suffix: ");
         suffix = scanner.nextLine();
+*/
 
 
-
-
-
-        System.out.println("Your Full Name is: " + firstName+", "+middleName+", "+lastName+" "+suffix +".");
-    }
 }
